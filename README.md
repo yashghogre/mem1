7c696d56e89f5a607f6cee773d2d1afdcc027944
yashghogre <yashghogre100@gmail.com>
2025-10-11 16:55:02 +0530

    chore: README update and main flow refactor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## mem1
 
-Replicating the mem0 paper for LLM memory and possibly trying to out-perform it XD
+Replicating the [mem0 paper](https://arxiv.org/pdf/2504.19413) for LLM memory and possibly trying to out-perform it XD
 
 ### Setup and initialization
 
--- src/main.py
+++ src/main.py
@@ -5,7 +5,12 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print("\n")
-    user_query = input("Enter you query: ")
-    result = inference(query=user_query)
-    print(f"\n> {result}")
+    stop_words = ["exit"]
+    while True:
+        print("\n")
+        user_query = input("Enter you query (Type 'exit' to quit): ")
+        if user_query.lower() in stop_words:
+            break
+        print(f"\nThinking...\n")
+        result = inference(query=user_query)
+        print(f"\n> {result}")
