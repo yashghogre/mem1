92b13696622ddb36b7e1a1585df34a1fe23bd8f6
yashghogre <yashghogre100@gmail.com>
2025-10-18 23:44:39 +0530

    refactor: changed method names and added timestamp on storing

--- infra/vector_db.py
+++ infra/vector_db.py
@@ -1,7 +1,8 @@
-import uuid
 import asyncio
+from datetime import datetime
 from qdrant_client import AsyncQdrantClient, models
 from typing import List
+import uuid
 
 from config import CONFIG
 from infra.embedder import Embedder
@@ -26,9 +27,11 @@ class VectorSearch:
             raise VectorSearchException(f"Error while setting up Vector Search (Collection creation)")
 
 
-    async def store_memory(self, text: str):
+    async def store_point(self, text: str):
         try:
             chunk_id = str(uuid.uuid4())
+            current_time = datetime.now()
+            time_now = current_time.strftime("%Y-%m-%d %H:%M:%S")
             chunk_vector = self.embedder.embed(text)
             await self.client.upsert(
                 collection_name=self.collection_name,
@@ -38,6 +41,7 @@ class VectorSearch:
                         vector=chunk_vector,
                         payload={
                             "text": text,
+                            "timestamp": time_now,
                         },
                     )
                 ],
@@ -47,17 +51,20 @@ class VectorSearch:
             raise VectorSearchException(f"Error while storing memory in Vector DB.")
 
 
-    async def store_memories(self, text: List[str]):
+    async def store_points(self, text: List[str]):
         try:
             chunk_vector = self.embedder.embed_batch(text)
             points_to_store = []
             for i, line in enumerate(text):
                 chunk_id = str(uuid.uuid4())
+                current_time = datetime.now()
+                time_now = current_time.strftime("%Y-%m-%d %H:%M:%S")
                 point_to_store = models.PointStruct(
                     id=chunk_id,
                     vector=chunk_vector[i],
                     payload={
                         "text": line,
+                        "timestamp": time_now,
                     },
                 )
                 points_to_store.append(point_to_store)
@@ -71,6 +78,6 @@ class VectorSearch:
             raise VectorSearchException(f"Error while storing memories in Vector DB.")
 
 
-    async def delete_memories(self):
+    async def delete_points(self):
         # Don't know yet what to delete, so leaving it empty for now.
         return
