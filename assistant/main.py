7ef5461cff770e71e7a373067ba2619c0831b4fb
yashghogre <yashghogre100@gmail.com>
2025-10-26 16:37:20 +0530

    fix: removed redis setup and utils

--- assistant/main.py
+++ assistant/main.py
@@ -5,29 +5,15 @@ from assistant.assistant import Assistant
 from assistant.deps.langfuse import init_langfuse
 from assistant.utils.logger import configure_logging
 from infra.database import DBStore
-from infra.redis import RedisClient
 
 @observe()
 async def main():
     configure_logging()
     await DBStore.init_db()
     init_langfuse()
-    RedisClient.connect()
     assistant = Assistant()
 
     print(f"Application started!")
-
-    '''
-    stop_words = ["exit"]
-    while True:
-        print("\n")
-        user_query = input("Enter your query (Type 'exit' to quit): ")
-        if user_query.lower() in stop_words:
-            break
-        print(f"\nThinking...\n")
-        result = inference(query=user_query)
-        print(f"\n> {result}")
-    '''
 
     user_query = input("Enter your query (Type '/exit' to quit): ")
     print(f"\nThinking...\n")
--- mem1/utils/redis_key_manager.py
+++
@@ -1,5 +0,0 @@
-from enum import StrEnum
-
-
-class RedisKeyManager(StrEnum):
-
