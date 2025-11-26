81221ac8a7b7a04ed8b1b60ad728ac48003c81c2
yashghogre <yashghogre100@gmail.com>
2025-11-26 18:59:31 +0530

    feat: memgraph setup + infra diverted to mem1

--- mem1/infra/graph_db.py
+++ mem1/infra/graph_db.py
@@ -1,5 +1,5 @@
 from neo4j import AsyncGraphDatabase
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from config import CONFIG
 
@@ -37,20 +37,19 @@ class GraphDB:
                 has_name = True
             new_dct[key.lower()] = value
         if not has_name:
-            # Not sure whether to raise the error or to silently suspend the entire memory operation.
             raise GraphDBException(f"Error while normalizing dict: No 'name' key found in Node.")
         return new_dct
 
 
-    # NOTE: We are storing every node with label `:Entity` for now for the simplicity of the code.
-    # Might change it later.
-    async def add_node(self, parameters: Dict[str, Any]):
+    async def add_node(self, parameters: Dict[str, Any], entity: Optional[str] = "Entity"):
         try:
-            # TIP: The node must have a parameter called 'name',
-            # remember this while crafting node making prompt.
+            # NOTE: Each node must have `Entity` type. And the entity parameters should have name,
+            # embedding vector and other metadata like timestamp, etc. (Keeping this here for future reference.)
+            # parameters = {"name" : "name-of-node", "vector_embedding": <embedding>, "timestamp": <timestamp>}
+
             parameters = self._normalize_dict(parameters)
             params_for_query = f"{{{", ".join(f"{key}: ${key}" for key, _ in parameters.items())}}}"
-            query = f"CREATE (e:Entity {params_for_query}) RETURN e"
+            query = f"CREATE (e:{entity} {params_for_query}) RETURN e"
 
             await self._execute_query(query=query, parameters=parameters)
 
@@ -58,12 +57,19 @@ class GraphDB:
             raise GraphDBException(f"Error while adding new node in GraphDB")
 
 
-    async def add_relationship(self, node_1_name: str, node_2_name: str, relationship: str):
+    async def add_relationship(
+        self,
+        node_1_name: str,
+        node_2_name: str,
+        relationship: str,
+        node_1_entity: Optional[str] = "Entity",
+        node_2_entity: Optional[str] = "Entity"
+    ):
         try:
             relationship = relationship.upper()
             query = f"""
-            MATCH (e1:Entity {{name: $node_1_name}})
-            MATCH (e2:Entity {{name: $node_2_name}})
+            MATCH (e1:{node_1_entity} {{name: $node_1_name}})
+            MATCH (e2:{node_2_entity} {{name: $node_2_name}})
             MERGE (e1)-[:{relationship}]->(e2)
             """
             parameters = {"node_1_name": node_1_name, "node_2_name": node_2_name}
@@ -90,7 +96,7 @@ class GraphDB:
             relationship = relationship.upper()
             query = f"""
             MATCH (e:Entity {{name: $name}})-[:{relationship}]->(rel_node:Entity)
-            RETURN friend
+            RETURN rel_node
             """
             parameters = {"name": name}
             return await self._execute_query(query=query, parameters=parameters)
